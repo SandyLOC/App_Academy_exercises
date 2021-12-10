62afba94a9f0a0ec6544f74e01868bfb1f0471fc
SandyLOC <sandrapl.18@gmail.com>
2021-12-09 22:00:43 -0800

    part_1 procs

--- procs_project/lib/part_1.rb
+++ procs_project/lib/part_1.rb
@@ -1,4 +1,44 @@
 def my_map(array, &prc)
-
+    new_arr = []
+    array.each {|ele| new_arr << prc.call(ele)}
+    new_arr
 end
 
+def my_select(array, &prc)
+    new_arr = []
+    array.each {|ele| new_arr << ele if prc.call(ele) == true}
+    new_arr
+end
+
+def my_count(array, &prc) 
+    count = 0
+    array.each {|ele| count += 1 if prc.call(ele) == true}
+    count
+end
+
+def my_any?(array, &prc)
+    array.each do |ele|
+        if prc.call(ele) == true
+            return true
+        end
+    end
+    false
+end
+
+def my_all?(array, &prc)
+    array.each do |ele|
+        if prc.call(ele) == false
+            return false
+        end
+    end
+    true
+end
+
+def my_none?(array, &prc)
+    array.each do |ele|
+        if prc.call(ele) == true
+            return false
+        end
+    end
+    true
+end
